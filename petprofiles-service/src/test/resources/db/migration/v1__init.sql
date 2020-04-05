2f0a8e4b197aad824323f467a85f3d962a23783f
Joffry Ferrater <joffry.ferrater@bisnode.com>
2020-04-05 22:20:58 +0200

    AB#1: Setup flyway and db init script

---
+++ petprofiles-service/src/test/resources/db/migration/v1__init.sql
@@ -0,0 +1,20 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- --------------------------------
+-- Table structure for pet_profiles
+-- --------------------------------
+DROP TABLE IF EXISTS `pet_profiles`;
+CREATE TABLE `pet_profiles` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `date_created` datetime DEFAULT NULL,
+  `date_updated` datetime DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `owner` varchar(255) NOT NULL,
+  `veterinarian` varchar(255) NOT NULL,
+  `clinic_location` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS = 1;
