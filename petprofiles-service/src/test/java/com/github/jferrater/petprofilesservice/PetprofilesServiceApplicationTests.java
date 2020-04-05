d3792a395a1e1ce3ef1ff16b71c24c10c204c2bb
Joffry Ferrater <joffry.ferrater@bisnode.com>
2020-04-05 17:54:00 +0200

    PetProfile service project setup

---
+++ petprofiles-service/src/test/java/com/github/jferrater/petprofilesservice/PetprofilesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jferrater.petprofilesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetprofilesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
