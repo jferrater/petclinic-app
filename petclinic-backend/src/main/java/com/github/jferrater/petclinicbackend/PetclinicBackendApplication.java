cd808b16d64ac8af8d90c64577cc152868fb2591
Joffry Ferrater <joffry.ferrater@bisnode.com>
2020-04-05 05:12:12 +0200

    Setup backend business microservice for Pet Clinic app

---
+++ petclinic-backend/src/main/java/com/github/jferrater/petclinicbackend/PetclinicBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.jferrater.petclinicbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetclinicBackendApplication.class, args);
+	}
+
+}
---
+++ petclinic-backend/src/test/java/com/github/jferrater/petclinicbackend/PetclinicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jferrater.petclinicbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
