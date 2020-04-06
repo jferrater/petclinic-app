74098cec6c89abf577914b8242c856107a57205b
Joffry Ferrater <joffry.ferrater@bisnode.com>
2020-04-06 22:52:59 +0200

    AB#1: Added flyway db init script

--- petprofiles-service/src/test/resources/db/migration/v1__init.sql
+++ petprofiles-service/src/test/resources/db/migration/v1__init.sql
@@ -18,3 +18,14 @@ CREATE TABLE `pet_profiles` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 SET FOREIGN_KEY_CHECKS = 1;
+
+INSERT INTO `pet_profiles` (`date_created`, `date_updated`, `name`, `owner`, `veterinarian`, `clinic_location`, `description`) VALUES(now(), now(), 'fluffy', 'dodong', 'alice', 'SOMA', 'brown color, hairy, cute, poodle breed');
+INSERT INTO `pet_profiles` (`date_created`, `date_updated`, `name`, `owner`, `veterinarian`, `clinic_location`, `description`) VALUES(now(), now(), 'browny', 'bob', 'alice', 'SOMA', 'brown color, st bernard breed');
+INSERT INTO `pet_profiles` (`date_created`, `date_updated`, `name`, `owner`, `veterinarian`, `clinic_location`, `description`) VALUES(now(), now(), 'chopper', 'joffry', 'alice', 'SOMA', 'dalmatian breed, black and white');
+INSERT INTO `pet_profiles` (`date_created`, `date_updated`, `name`, `owner`, `veterinarian`, `clinic_location`, `description`) VALUES(now(), now(), 'soba', 'jolly', 'alice', 'SOMA', 'black, hairy, siberian husky breed');
+INSERT INTO `pet_profiles` (`date_created`, `date_updated`, `name`, `owner`, `veterinarian`, `clinic_location`, `description`) VALUES(now(), now(), 'samsam', 'joffry', 'wella', 'VETE', 'white with black dots, terrier breed');
+INSERT INTO `pet_profiles` (`date_created`, `date_updated`, `name`, `owner`, `veterinarian`, `clinic_location`, `description`) VALUES(now(), now(), 'chiko', 'mongkoy', 'wella', 'VETE', 'small askal breed, white');
+INSERT INTO `pet_profiles` (`date_created`, `date_updated`, `name`, `owner`, `veterinarian`, `clinic_location`, `description`) VALUES(now(), now(), 'hatchi', 'alvin', 'wella', 'VETE', 'labrador breed, yellowish');
+INSERT INTO `pet_profiles` (`date_created`, `date_updated`, `name`, `owner`, `veterinarian`, `clinic_location`, `description`) VALUES(now(), now(), 'kaikai', 'joy', 'wella', 'VETE', 'askal breed, orange color');
+INSERT INTO `pet_profiles` (`date_created`, `date_updated`, `name`, `owner`, `veterinarian`, `clinic_location`, `description`) VALUES(now(), now(), 'nicole', 'justin', 'wella', 'VETE', 'golden retriever, white');
+INSERT INTO `pet_profiles` (`date_created`, `date_updated`, `name`, `owner`, `veterinarian`, `clinic_location`, `description`) VALUES(now(), now(), 'whitey', 'debby', 'alice', 'SOMA', 'beagle breed, brown white and black color');
