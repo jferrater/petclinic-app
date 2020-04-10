a16776e8830cfbd7becf8937681b67228a797179
Joffry Ferrater <joffry.ferrater@bisnode.com>
2020-04-10 22:28:57 +0200

    AB#7: * Use H2 database as default datasource      * Updated docker-compose

--- petprofiles-service/src/test/java/com/github/jferrater/petprofilesservice/controller/PetProfilesControllerTest.java
+++ petprofiles-service/src/test/java/com/github/jferrater/petprofilesservice/controller/PetProfilesControllerTest.java
@@ -13,7 +13,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -34,7 +33,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
 class PetProfilesControllerTest {
 
     @Autowired
